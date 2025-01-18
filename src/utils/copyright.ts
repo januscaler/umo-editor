ebadbed9b80dd56e55df3af5eecd362042e693bd
shivanshtalwar0 <shivanshtalwar0@gmail.com>
2025-01-18 08:17:42 +0200

    fixes

--- src/utils/copyright.ts
+++ src/utils/copyright.ts
@@ -1,16 +1,10 @@
 import pkg from '../../package.json'
 
-export default `/**
- * ${pkg.name} ${pkg.version}
- * @license ${pkg.license}
- * @author ${pkg.author.name} ${pkg.author.url}
- * @see ${pkg.homepage}
- **/
-`
+export default ``
 
 export const consoleCopyright = () => {
   console.info(
-    t('welcome', { version: pkg.version, homepage: pkg.homepage }),
+    t('welcome', { version: pkg.version, homepage: '' }),
     'background:#3480f9;color:#fff;border-top-left-radius:3px;border-bottom-left-radius:3px;padding:4px 8px 3px;',
     'background:#fff;color:#3480f9;border-top-right-radius:3px;border-bottom-right-radius:3px;border:solid 1px #3480f9;padding:3px 8px 2px;',
   )
